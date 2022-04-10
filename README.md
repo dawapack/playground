082587a8d8e235dc557fad090c61de600bc9d9c0
cezar.vant <cezar.vant@123fombuilder.com>
2022-04-10 14:20:02 +0300

    Initial commit - ready to play

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# playground
+# DaWaPack Playground
 The DaWaPack Playground is a Docker Compose environment that puts together the necessary pieces to let you play with DaWaPack in a demo environment.
+
+## Install
+
+```
+bash-5.1$ git clone git@github.com:dawapack/playground.git
+bash-5.1$ cd playground
+bash-5.1$ ./bin/playground install
+```
+
+## Start playground
+
+```
+bash-5.1$ ./bin/playground up
+```
+
+## Stop playground
+
+```
+bash-5.1$ ./bin/playground down
+```
+
+## Containers
+
+- **RabbitMQ**
+- **Redis** - act as database for authors and posts microservices
+- **Infrastructure** - this container will setup queues, exchanges and bindings
+- **Authors** - see playground/api/authors.http for api calls
+- **Posts** - see playground/api/posts.http for api calls
+
+```
+bash-5.1$ docker exec -it (infrastructure|authors|posts)_dms bash
+```
+
+## RabbitMq Management
+
+Just `http://localhost:15672/` (**user**: guest & **password**: guest)
+
+## Redis GUI Tool
+
+Just connect with your favorite redis tool to `localhost:6379`
---
+++ var/dms/settings/infrastructure/config/threads.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace DaWaPack\Config;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default settings
+    |--------------------------------------------------------------------------
+    */
+    // minimum vertical scaling
+    'minimum' => 2,
+    // maximum vertical scaling
+    'maximum' => 30,
+    // triggers used by scaling mechanism
+    'triggers' => [
+        35 => 1,
+        55 => 2,
+        75 => 5,
+        90 => 10,
+    ],
+    // How many times the thread will live - default: 1 hour
+    'ttl' => 3600,
+    // Haw many jobs before restart the thread - default: 1800
+    'max_jobs' => 1800,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Infrastructure default thread settings
+    |--------------------------------------------------------------------------
+    */
+    'infrastructure' => [
+        'minimum' => 1,
+        'maximum' => 1,
+        'enabled' => true
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Centralized configuration default thread settings
+    |--------------------------------------------------------------------------
+    */
+    'configuration' => [
+        'minimum' => 1,
+        'maximum' => 1,
+        'enabled' => false
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Worker thread(s) default settings
+    |--------------------------------------------------------------------------
+    */
+    'worker' => [
+        'channels' => [],
+        'enabled' => false
+    ],
+];
